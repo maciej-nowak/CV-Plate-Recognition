a9ca5c840ba41f06c313b9fa48135884c7a96937
madjer22 <maciej.nowak.zslg@gmail.com>
2016-11-04 15:48:58 +0100

    Histogram calculating added

--- app/src/main/java/pl/maciej_nowak/lwk/LWK4.java
+++ app/src/main/java/pl/maciej_nowak/lwk/LWK4.java
@@ -10,13 +10,25 @@ import android.widget.ImageView;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class LWK4 extends AppCompatActivity {
 
-    ImageView imageOriginal, imageEroded, imageDilated;
+    ImageView imageOriginal, imageEroded, imageDilated, imageHistogramCalculated;
     Mat image;
 
     @Override
@@ -29,6 +41,7 @@ public class LWK4 extends AppCompatActivity {
         imageOriginal = (ImageView) findViewById(R.id.imageOriginal);
         imageEroded = (ImageView) findViewById(R.id.imageEroded);
         imageDilated = (ImageView) findViewById(R.id.imageDilated);
+        imageHistogramCalculated = (ImageView) findViewById(R.id.imageHistogramCalculated);
 
     }
 
@@ -53,6 +66,7 @@ public class LWK4 extends AppCompatActivity {
                     loadImage();
                     erodeImage(5);
                     dilateImage(5);
+                    calculateHistogram();
                 } break;
                 default: {
                     super.onManagerConnected(status);
@@ -79,7 +93,29 @@ public class LWK4 extends AppCompatActivity {
         Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2*size+1, 2*size+1));
         Imgproc.dilate(image, dilatedImage, element);
         OpenCV.saveImage("dilatedImage.jpg", dilatedImage);
+        Log.d("TAG", "D: " + dilatedImage.toString());
         imageDilated.setImageBitmap(OpenCV.matToBitmap(dilatedImage));
+    }
+
+    private void calculateHistogram() {
+        MatOfInt size = new MatOfInt(256);
+        MatOfFloat range = new MatOfFloat(0f, 256f);
+        MatOfInt[] channels = new MatOfInt[]{new MatOfInt(0), new MatOfInt(1), new MatOfInt(2)};
+        Scalar[] colorsRgb = new Scalar[]{new Scalar(200, 0, 0), new Scalar(0, 200, 0), new Scalar(0, 0, 200)};
+        Mat[] histograms = new Mat[]{new Mat(), new Mat(), new Mat()};
+        Mat histogram = new Mat(image.size(), image.type());
+
+        for(int i=0; i<channels.length; i++) {
+            Imgproc.calcHist(Collections.singletonList(image), channels[i], new Mat(), histograms[i], size, range);
+            Core.normalize(histograms[i], histograms[i], image.height(), 0, Core.NORM_INF);
+            for(int j=0; j<256; j++) {
+                Point p1 = new Point(5*(j-1), image.height() - Math.round(histograms[i].get(j-1, 0)[0]));
+                Point p2 = new Point(5*j, image.height() - Math.round(histograms[i].get(j, 0)[0]));
+                Imgproc.line(histogram, p1, p2, colorsRgb[i], 2, 8, 0);
+            }
+        }
+        OpenCV.saveImage("histogramImage.jpg", histogram);
+        imageHistogramCalculated.setImageBitmap(OpenCV.matToBitmap(histogram));
     }
 
 }
